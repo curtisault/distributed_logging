258b21e8aa5836dc071d59f2abeff8aa5ee11acb
Curtis Ault <curtisault@gmail.com>
2020-11-07 14:17:52 -0600

    modified run script

--- run.sh
+++ run.sh
@@ -3,9 +3,10 @@ set -euo pipefail
 
 echo "Getting deps"
 
-# mix deps.get
-# mix deps.compile
-# mix compile
+rm -rf _build/ deps/ node/
+mix deps.get
+mix deps.compile
+mix compile
 
 echo "Running Elixir. Run ./test.sh in new session."
 mix run --no-halt
